6dbccdd48bcfe2eed6ed0cb297546e11eb948d24
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-04-23 19:25:51 +0900

    first commit

---
+++ js/calc.js
@@ -0,0 +1,13 @@
+function calc() {
+  var x = document.getElementById('dollar');
+  var i = x.selectedIndex + 1;
+  var weekly = document.getElementById('weekly-checked').checked;
+    if (weekly) {
+      var sum = i * 752;
+    } else {
+      var sum = i * 173;
+    }
+  document.getElementById('output').innerHTML = '$' + sum;
+}
+
+calc();
